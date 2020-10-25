067a65c652bca5a1c9e4fcc3267cee764c627913
aliashour <aliashour592@gmail.com>
2020-10-25 16:21:28 +0200

    another Modifayed

--- js/main.js
+++ js/main.js
@@ -94,5 +94,13 @@ $(function(){
     
     }) ;
     
+    //Turn on NiceScroll Plugin 
 
+    $("body").niceScroll({
+        cursorcolor:"#10cab0",
+        cursorwidth:"12px",
+        zindex: 999999, 
+        scrollspeed: 60, 
+        mousescrollstep: 60
+    });
 })  
